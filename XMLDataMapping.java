5ed994f7cc5fb3759f29a44fff9c67709ab48514
sathish natarajan <natarajansathish@gmail.com>
2012-05-16 15:35:43 +0530

    xml data mapping commit

---
+++ XMLDataMapping.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author sathishnataraj
+ * This is class added as a template to store and used for mapping the data 
+ * to generate the xml
+ */
+
+public  class  XMLDataMapping 
+{
+    
+    public final String ZERO = "0";
+    public final String ONE = "1";
+    public final String TWO = "2";
+    public final String THREE = "3";
+    public final String FOUR = "4";
+                
+}
